52baae477bb104d242c69692674f8830a2c40248
LeeSeungHyun <iop2480@naver.com>
2022-05-03 09:19:07 +0900

    rest api 기본 작업(CRUD)

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const { ObjectId } = require('mongodb')
 const { connectToDb, getDb } = require('./db')
 
 // init app & miidleware
 const app = express()
+app.use(express.json())
 
 // db connection
 let db
@@ -30,4 +32,64 @@ app.get('/books', (req, res) => {
     .catch(() => {
       res.status(500).json({ error: 'Could not fetch the documents' })
     })
+})
+
+app.get('/books/:id', (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection('books')
+    .findOne({_id: ObjectId(req.params.id)})
+    .then(doc => {
+      res.status(200).json(doc)
+    })
+    .catch(err => {
+      res.status(500).json({error: 'Could not fetch the document'})
+    })
+  } else {
+    res.status(500).json({ error: 'Not a valid doc id'})
+  }
+})
+
+app.post('/books', (req, res) => {
+  const book = req.body
+
+  db.collection('books')
+    .insertOne(book)
+    .then(result => {
+      res.status(201).json(result)
+    })
+    .catch(err => {
+      res.status(500).json({err: 'Could not create a new document'})
+    })
+})
+
+app.delete('/books/:id', (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection('books')
+    .deleteOne({_id: ObjectId(req.params.id)})
+    .then(result => {
+      res.status(200).json(result)
+    })
+    .catch(err => {
+      res.status(500).json({error: 'Could not delete the document'})
+    })
+  } else {
+    res.status(500).json({ error: 'Not a valid doc id'})
+  }
+})
+
+app.patch('/books/:id', (req, res) => {
+  const updates = req.body
+
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection('books')
+    .updateOne({_id: ObjectId(req.params.id)}, {$set: updates})
+    .then(result => {
+      res.status(200).json(result)
+    })
+    .catch(err => {
+      res.status(500).json({error: 'Could not update the document'})
+    })
+  } else {
+    res.status(500).json({ error: 'Not a valid doc id'})
+  }
 })
